0539e5e81a021d6b79963484b7abae750caf845b
Pythonikk <nikkibrusoe@gmail.com>
2022-05-22 15:38:59 -0700

    Add flash messages and validations.

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -15,8 +15,10 @@ class KittensController < ApplicationController
     @kitten = Kitten.new(kitten_params)
     if @kitten.save
       redirect_to @kitten
+      flash.notice = 'You have successfully added a kitten'
     else
-      render :new, status: :unprocessible_entity
+      render :new, status: :unprocessable_entity
+      flash.alert = 'We cannot accept your kitten'
     end
   end
 
@@ -27,8 +29,10 @@ class KittensController < ApplicationController
   def update
     if @kitten.update(kitten_params)
       redirect_to @kitten
+      flash.notice = 'You have successfully updated your kitten'
     else
-      render :edit, status: :unprocessible_entity
+      render :edit, status: :unprocessable_entity
+      flash.alert = 'We cannot accept your kitten'
     end
   end
 
@@ -36,6 +40,7 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
     @kitten.destroy
     redirect_to root_path, status: 303
+    flash.notice = 'You have successfully deleted your kitten'
   end
 
   private
